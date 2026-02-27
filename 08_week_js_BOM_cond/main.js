6ec2ab7e843939d2c98385618ff3f5bac9a6670f
Youssef El Sayed <youssef.el.sayed.email@gmail.com>
2026-02-27 23:25:00 +0200

    upload 08_week

---
+++ 08_week_js_BOM_cond/main.js
@@ -0,0 +1,45 @@
+// // function fetchData(cb) {
+// //   // Simulate API Call (Network)
+// //   setTimeout(function () {
+// //     const data = {
+// //       name: "Youssef",
+// //       job: "Developer",
+// //     };
+
+// //     cb(data);
+// //   }, 2000);
+// // }
+
+// // // const userData = fetchData();
+
+// // // console.log({
+// // //   userData,
+// // // });
+
+// // const cb = function (d) {
+// //   const userData = d;
+
+// //   console.log("Inside Callback Function", userData);
+// // };
+
+// // fetchData(cb);
+
+// // /**
+// //  * function => enter call stack => out of call stack
+// //  */
+
+// // console.log("Inner Width", window.innerWidth);
+
+// /**
+//   window.location.href returns the href (URL) of the current page
+//   window.location.hostname returns the domain name of the web host
+//   window.location.pathname returns the path and filename of the current page
+//   window.location.protocol returns the web protocol used (http: or https:)
+//   window.location.assign()
+//  */
+
+// // console.log("window.location.href", window.location.href);
+// // console.log("window.location.hostname", window.location.hostname);
+// // console.log("window.location.pathname", window.location.pathname);
+// // console.log("window.location.protocol", window.location.protocol);
+// // console.log("window.history.length", window.history.length);
